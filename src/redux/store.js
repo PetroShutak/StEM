18ddae92f63a215199077dd4f0a52d0af268ad8d
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-12-04 21:08:45 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,48 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 import {
-	persistStore,
-	persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from 'redux-persist';
-import storage from "redux-persist/lib/storage";
-import { productsReducer } from "./products/productSlice";
-import { filterReducer} from "./products/filterSlice";
-import { favoriteReducer } from "./products/favoriteSlice";
-import { shoppingListReducer } from "./products/shoppingListSlice";
+import storage from 'redux-persist/lib/storage';
+import { productsReducer } from './products/productSlice';
+import { filterReducer } from './products/filterSlice';
+import { favoriteReducer } from './products/favoriteSlice';
+import { shoppingListReducer } from './products/shoppingListSlice';
 
 const persistConfig = {
-  	key: "favorites",
-  	storage,
-  	whitelist: ["favorites"],
+  key: 'root',
+  storage,
+  whitelist: ['favorites', 'shoppingList'],
 };
 
 const persistedFavoriteReducer = persistReducer(persistConfig, favoriteReducer);
+const persistedShoppingListReducer = persistReducer(
+  persistConfig,
+  shoppingListReducer
+);
 
 export const store = configureStore({
-	reducer: {
-		products: productsReducer,
-		filter: filterReducer,
-		favorites: persistedFavoriteReducer,
-		shoppingList: shoppingListReducer,
-	},
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [
-					FLUSH,
-					REHYDRATE,
-					PAUSE,
-					PERSIST,
-					PURGE,
-					REGISTER,
-				],
-			},
-		}),
+  reducer: {
+    products: productsReducer,
+    filter: filterReducer,
+    favorites: persistedFavoriteReducer,
+    shoppingList: persistedShoppingListReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
