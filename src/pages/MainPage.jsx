cd1f1c79f557de7f3f55736906f50a421a90e44c
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-12-04 23:20:39 +0200

    add slider for example

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 const MainPage = () => {
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1021/1000/600/",
+      thumbnail: "https://picsum.photos/id/1021/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1022/1000/600/",
+      thumbnail: "https://picsum.photos/id/1022/250/150/",
+    }
+   
+  ];
   return (
-    <div
+    <>
+    <ImageGallery items={images} 
+    showFullscreenButton={false}
+    showPlayButton={false}
+    showThumbnails={false}
+    showBullets={false}
+    showNav={false}
+    autoPlay={true}
+    slideInterval={5000}
+    lazyLoad={true}
+    slideDuration={1000}
+    />
+    
+    {/* <div
       style={{
         paddingTop: '40px',
         paddingBottom: '40px',
@@ -11,7 +49,8 @@ const MainPage = () => {
       <h1 style={{
         color: 'var(--text-color-primary-black)'
       }}>Головна</h1>
-    </div>
+    </div> */}
+      </>
   );
 };
 
