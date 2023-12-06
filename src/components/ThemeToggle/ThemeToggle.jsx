b40a2f89a4fb13b3adbaeae32ed2ee76711207f6
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-12-06 13:54:01 +0200

    change toggler-theme

--- src/components/ThemeToggle/ThemeToggle.jsx
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -1,6 +1,7 @@
 import { useTheme } from 'hooks/useTheme';
 import { ThemeToggleContainer, ThemeToggleIcon } from './ThemeToggle.styled';
-import { PiToggleLeftFill, PiToggleRightFill } from "react-icons/pi";
+// import { PiToggleLeftFill, PiToggleRightFill } from "react-icons/pi";
+import { MdOutlineLightMode, MdOutlineNightlight,  } from 'react-icons/md';
 
 const ThemeToggle = () => {
   const [ theme, setTheme ] = useTheme();
@@ -9,17 +10,20 @@ const ThemeToggle = () => {
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light');
     // console.log(theme);
-  };
 
+  };
 
 
   return (
     <>
       <ThemeToggleContainer onClick={toggleTheme}>
-        
-        <ThemeToggleIcon>
-          {theme === 'light' ? <PiToggleLeftFill size={36} color='#000' /> : <PiToggleRightFill size={36} color='white'/>}
+      <ThemeToggleIcon>
+          {theme === 'light' ? <MdOutlineNightlight size={32} color='#000' /> : <MdOutlineLightMode size={32} color='white'/>}
         </ThemeToggleIcon>
+        {/* <span>{theme}</span> */}
+        {/* <ThemeToggleIcon>
+          {theme === 'light' ? <PiToggleLeftFill size={36} color='#000' /> : <PiToggleRightFill size={36} color='white'/>}
+        </ThemeToggleIcon> */}
         
       </ThemeToggleContainer>
     </>
--- src/components/ThemeToggle/ThemeToggle.styled.jsx
+++ src/components/ThemeToggle/ThemeToggle.styled.jsx
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
 
-export const ThemeToggleContainer = styled.div``;
+export const ThemeToggleContainer = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  span {
+    font-family: var(--font-family-secondary);
+    font-size: 12px;
+    color: var(--text-color-primary-black);
+  }
+`;
 
 export const ThemeToggleIcon = styled.div`
   cursor: pointer;
   font-size: 1.5rem;
-transition: all 0.3s linear;
+  transition: all 0.3s linear;
 `;
