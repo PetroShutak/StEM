38c04ae5a64c9e805a88f5311e43c0db30d183ff
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-12-17 01:02:21 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import Footer from 'components/Footer/Footer';
+// import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 import Loader from 'components/Loader/Loader';
 import React from 'react';
