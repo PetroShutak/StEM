8f30277074c7e1296b68233c4011881f59ea07c9
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-12-21 22:15:55 +0200

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,44 +1,43 @@
 import BrandCarousel from 'components/BrandCarousel/BrandCarousel';
 import React from 'react';
-import ImageGallery from 'react-image-gallery';
+// import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
 const MainPage = () => {
-  const images = [
-    {
-      original: 'https://picsum.photos/id/1018/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1018/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1015/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1015/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1019/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1019/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1021/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1021/250/150/',
-    },
-    {
-      original: 'https://picsum.photos/id/1022/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1022/250/150/',
-    },
-  ];
+  // const images = [
+  //   {
+  //     original: 'https://picsum.photos/id/1018/1000/600/',
+  //     thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  //   },
+  //   {
+  //     original: 'https://picsum.photos/id/1015/1000/600/',
+  //     thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  //   },
+  //   {
+  //     original: 'https://picsum.photos/id/1019/1000/600/',
+  //     thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  //   },
+  //   {
+  //     original: 'https://picsum.photos/id/1021/1000/600/',
+  //     thumbnail: 'https://picsum.photos/id/1021/250/150/',
+  //   },
+  //   {
+  //     original: 'https://picsum.photos/id/1022/1000/600/',
+  //     thumbnail: 'https://picsum.photos/id/1022/250/150/',
+  //   },
+  // ];
   return (
     <>
-      <BrandCarousel />
-    <p style={{
-      textAlign: 'center',
-      fontSize: '24px',
-      fontWeight: 'bold',
-      color: 'var(--text-color-primary-black)',
-      paddingTop: '40px',
-      paddingBottom: '40px',
-    }}
-    >Тут будуть слайди основні, а поки такий слайдер</p>
-      <ImageGallery
+      <div
+        style={{
+          paddingTop: '40px',
+          paddingBottom: '40px',
+        }}
+      >
+    
+          <BrandCarousel />
+      </div>
+      {/* <ImageGallery
         items={images}
         showFullscreenButton={false}
         showPlayButton={false}
@@ -49,7 +48,7 @@ const MainPage = () => {
         slideInterval={5000}
         lazyLoad={true}
         slideDuration={1000}
-      />
+      /> */}
       {/* <div
       style={{
         paddingTop: '40px',
