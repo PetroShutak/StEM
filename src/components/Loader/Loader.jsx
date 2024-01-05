e3f44d437e15a61b5f4b008422a6f43978aa33ed
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2024-01-05 12:43:08 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -10,6 +10,7 @@ const Loader = () => {
         width: '100vw',
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         background: 'rgba(0, 0, 0, 0.5)',
