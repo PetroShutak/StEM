1b650beb2fbf6747e14ac5c5895d541f95b1acc2
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2024-01-05 12:30:20 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -26,6 +26,9 @@ const Loader = () => {
         borderColor="#e24617"
         barColor="#ff6b09"
       />
+       <p style={{ marginTop: '20px', color: '#ff6b09', fontSize: '14px' }}>
+        Завантажується...
+      </p>
     </div>
   );
 };
