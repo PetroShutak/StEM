213b01b9dc5a305aa800ad01480c2edf074c8623
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2024-01-23 16:10:17 +0200

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,13 +2,6 @@ import BrandCarousel from 'components/BrandCarousel/BrandCarousel';
 import React from 'react';
 
 const MainPage = () => {
-    const [viewedProducts, setViewedProducts] = useState([]);
-
-  useEffect(() => {
-    // Отримання переглянутих товарів з локального сховища
-    const storedViewedProducts = JSON.parse(localStorage.getItem('viewedProducts')) || [];
-    setViewedProducts(storedViewedProducts);
-  }, []);
   return (
     <>
       <div
@@ -18,18 +11,6 @@ const MainPage = () => {
         }}
       >
         <BrandCarousel />
-      </div>
-      <div>
-        <h2>Нещодавно переглянуті</h2>
-        <ul>
-          {viewedProducts.map((product) => (
-            <li key={product._id}>
-              <img src={product.image} alt={product.name} style={{ maxWidth: '100px' }} />
-              <span>{product.name}</span>
-              <span>Price: {product.price}</span>
-            </li>
-          ))}
-        </ul>
       </div>
     </>
   );
