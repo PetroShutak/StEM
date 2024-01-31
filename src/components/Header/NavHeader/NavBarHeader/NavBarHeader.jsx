0f704b63d4c6c55ff5a75d200393ced4e8c6d7f0
Petro Shutak <petro.shutak.ua@gmail.com>
2024-01-31 17:03:05 +0200

    more-link

--- src/components/Header/NavHeader/NavBarHeader/NavBarHeader.jsx
+++ src/components/Header/NavHeader/NavBarHeader/NavBarHeader.jsx
@@ -3,14 +3,17 @@ import { NavLink } from 'react-router-dom';
 import { NavBarHeaderWrapper } from './NavBarHeader.styled';
 
 const NavBarHeader = () => {
-
   return (
     <NavBarHeaderWrapper>
       <NavLink to="/catalog">Каталог</NavLink>
       <NavLink to="/servis">Сервісний центр</NavLink>
-      <NavLink to="/delivery">Доставка і оплата</NavLink>
-      <NavLink to="/about">Про нас</NavLink>
-      <NavLink to="/contacts">Контактна інформація</NavLink>
+      <div className="more-link">
+        <div className="dropdown">
+          <NavLink to="/delivery">Доставка і оплата</NavLink>
+          <NavLink to="/about">Про нас</NavLink>
+          <NavLink to="/contacts">Контактна інформація</NavLink>
+        </div>
+      </div>
     </NavBarHeaderWrapper>
   );
 };
--- src/components/Header/NavHeader/NavBarHeader/NavBarHeader.styled.jsx
+++ src/components/Header/NavHeader/NavBarHeader/NavBarHeader.styled.jsx
@@ -20,8 +20,59 @@ export const NavBarHeaderWrapper = styled.div`
       color: var(--text-color-primary-orange);
     }
   }
+
+  .dropdown {
+    display: flex;
+    gap: 20px;
+    align-items: center;
+  }
+
   @media (max-width: 1024px) {
+    a:nth-child(n+3) {
+      display: none;
+    }
+    
+    .more-link {
+      display: flex;
+      align-items: center;
+      gap: 20px; 
+      position: relative;
+
+      &:before {
+        content: 'Ще...';
+        font-family: var(--font-family-secondary);
+        color: var(--text-color-primary-black);
+        font-size: 16px;
+        font-weight: 500;
+        text-decoration: none;
+        margin-right: 10px;
+      }
+
+      .dropdown {
+        display: none;
+        flex-direction: column;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        background-color: var(--bg-primary);
+        border: 1px solid var(--border-color-secondary);
+        padding: 10px;
+        z-index: 1;
+        gap: 20px; 
+
+        a {
+          display: block;
+          width: 200px;
+        }
+      }
+
+      &:hover .dropdown {
+        display: flex;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
     display: none;
   }
 `;
-
