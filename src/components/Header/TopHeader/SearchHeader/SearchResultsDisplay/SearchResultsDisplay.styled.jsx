7c86fbcf0c52254d274762693fffb68e49f65e6f
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2024-02-19 22:06:15 +0200

    Update SearchResultsDisplay.styled.jsx

--- src/components/Header/TopHeader/SearchHeader/SearchResultsDisplay/SearchResultsDisplay.styled.jsx
+++ src/components/Header/TopHeader/SearchHeader/SearchResultsDisplay/SearchResultsDisplay.styled.jsx
@@ -2,9 +2,12 @@ import styled from 'styled-components';
 
 
 export const SearchList = styled.ul`
-  list-style: none;
-  padding: 0;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-  gap: 20px;
+    display: flex;
+    gap: 40px;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    list-style: none;
     `;
